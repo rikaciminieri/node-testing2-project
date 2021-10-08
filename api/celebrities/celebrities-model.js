825d48eedecd1bc4c9ddc1e4f62ff2d32ee6e96a
Rika Ciminieri <rikaciminieri@gmail.com>
2021-10-07 20:54:16 -0700

    Model testing complete

--- api/celebrities/celebrities-model.js
+++ api/celebrities/celebrities-model.js
@@ -1,1 +1,26 @@
 const db = require("../../data/db-config")
+
+function getAll() {
+    return db("celebrities")
+}
+
+function getById(id) {
+    return db("celebrities").where("celebrity_id", id).first()
+}
+
+async function insert(celebrity) {
+    return db("celebrities").insert(celebrity)
+    .then(([id]) => {
+        return getById(id)
+    })
+}
+
+function remove(id) {
+    return db("celebrities").where("celebrity_id", id).del()
+}
+
+module.exports = {
+    getAll,
+    insert,
+    remove
+}
